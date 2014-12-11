361f95cff97446243a9618e702ee298f1e66df24
Jim Lawton <jim.lawton@gmail.com>
2014-12-11 17:31:11 +0000

    Revert those changes.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 import sys
 import os
-import ssl
-
 
 plugin = os.path.abspath(os.path.split(__file__)[0])
 libpath = os.path.join(plugin, 'lib')
@@ -14,11 +12,3 @@ if not libpath in sys.path:
   sys.path.append(libpath)
 
 VERSION = '0.1.4'
-
-import requests
-from requests.packages import urllib3 
-
-from imp import reload
-reload(requests)
-reload(urllib3)
-
